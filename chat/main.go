7e6d7918d9ff51bc483db4367fc631a6757337d8
yuya <cheeeese4869@gmail.com>
2021-09-23 15:53:54 +0900

    signin with google

--- chat/main.go
+++ chat/main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"sync"
 	"text/template"
+
+	"github.com/joho/godotenv"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
 )
 
 type templateHandler struct {
@@ -15,19 +23,34 @@ type templateHandler struct {
 	templ    *template.Template
 }
 
+// ServeHTTPはHTTPリクエストを処理します
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
-		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+		t.templ =
+			template.Must(template.ParseFiles(filepath.Join("templates",
+				t.filename)))
 	})
 	t.templ.Execute(w, r)
 }
 
 func main() {
+	err := godotenv.Load(".env")
+	fmt.Println(err)
+	google_client_id := os.Getenv("GOOGLE_CLIENT_ID")
+	google_client_secret := os.Getenv("GOOGLE_CLIENT_SECRET")
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse()
+	gomniauth.SetSecurityKey("gosecuritykey")
+	gomniauth.WithProviders(
+		facebook.New("クライアントID", "秘密の値", "http://localhost:8080/auth/callback/facebook"),
+		github.New("クライアントID", "秘密の値", "http://localhost:8080/auth/callback/github"),
+		google.New(google_client_id, google_client_secret, "http://localhost:8080/auth/callback/google"),
+	)
 	r := newRoom()
-	// r.tracer = trace.New(os.Stdout)
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	// r.tracer = trace.New(os.Stdout) でターミナルにログを出力
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	// チャットルームを開始します
 	go r.run()
