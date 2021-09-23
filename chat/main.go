424b1a5fcae5487d89a9f8ea8bb4cdbcef04a80d
yuya <cheeeese4869@gmail.com>
2021-09-23 14:18:54 +0900

    socket chat

--- chat/main.go
+++ chat/main.go
@@ -16,16 +16,18 @@ type templateHandler struct {
 
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
-		t.templ =
-			template.Must(template.ParseFiles(filepath.Join("templates",
-				t.filename)))
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
 	t.templ.Execute(w, r)
 }
 
 func main() {
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
-
+	http.Handle("/room", r)
+	// チャットルームを開始します
+	go r.run()
+	// Webサーバーを起動します
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
